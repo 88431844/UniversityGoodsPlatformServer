38cec9b1dbcb96c7b53bbcfc93864d24d536f068
neusoft-work-pc <88431844@163.com>
2016-12-22 12:07:24 +0800

    修改注释

--- ugp/src/main/java/server/adminInfo/controller/AdminInfoController.java
+++ ugp/src/main/java/server/adminInfo/controller/AdminInfoController.java
@@ -1,11 +1,14 @@
 package server.adminInfo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import server.adminInfo.dao.AdminInfoMapper;
+
 /**
  * 管理员controller
- * @author:高治华
+ * @author:miracle
  * Created on 2016年12月19日 上午9:44:47
  * @modify author:修改人
  * Modify on 修改时间
@@ -14,4 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/adminInfo")
 public class AdminInfoController {
 
+	@Autowired
+	private AdminInfoMapper adminInfoMapper;
 }
--- ugp/src/main/java/server/userInfo/controller/UserInfoController.java
+++ ugp/src/main/java/server/userInfo/controller/UserInfoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import server.userInfo.service.UserInfoServer;
 /**
  * 用户controller
- * @author:高治华
+ * @author:miracle
  * Created on 2016年12月13日 上午10:55:35
  * @modify author:修改人
  * Modify on 修改时间
